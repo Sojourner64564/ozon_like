a186c07aab7773ca25fb32186ffdbbea66e6a62f
Sojourner64564 <122652562+Sojourner64564@users.noreply.github.com>
2023-01-20 03:47:19 +0300

    commit

--- lib/Cubits/change_tile_of_ozon_point_animated_size_cubit.dart
+++ lib/Cubits/change_tile_of_ozon_point_animated_size_cubit.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChangeTileOfOzonPointAnimatedSize extends Cubit<int>{
-  ChangeTileOfOzonPointAnimatedSize() : super(64);
+  ChangeTileOfOzonPointAnimatedSize() : super(49);
   void hui(double chislo){
-    if(chislo<64)
-    emit(64-chislo.toInt());
+    if(chislo<49)
+    emit(49-chislo.toInt());
 
-    if(chislo>64)
+    if(chislo>49)
       emit(0);
 
 
